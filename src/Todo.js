4bce7c8378ebaa9d37254b77c5c04d3951a76272
Lee Soo Yeon <lala555@naver.com>
2023-04-18 03:53:21 +0900

    8. React에서 리스트 렌더링하기(1)

--- src/Todo.js
+++ src/Todo.js
@@ -5,17 +5,17 @@ class Todo extends React.Component {
         super(props);
         this.state ={ item: props.item };  // 매개변수 item 의 변수/값을 item에 대입
     }
+
     render(){
         return (
             <div className="Todo">
                 <input
                     type="checkbox"
-                    id={this.state.item.id}
+                    id={this.state.item.id} // item.id 값으로 렌더링하란 의미(JPX)
                     name={this.state.item.id}
-                    checked={this.state.item.done}
+                    checked={this.state.item.done} // item.done 값으로 렌더링하란 의미미
                 />
                 <label for={this.state.item.id}>{this.state.item.title}</label>
-
             </div>
         );
     }
