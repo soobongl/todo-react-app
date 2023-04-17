4928d0e0f493d055e5995e5bb1ac11f233d1c38e
Lee Soo Yeon <lala555@naver.com>
2023-04-18 04:25:23 +0900

    10. React 로 목록 추가, 수정, 삭제하기

--- src/App.js
+++ src/App.js
@@ -24,7 +24,16 @@ class App extends React.Component {
     console.log("items:", this.state.items);
 
   }
-  
+  // delete 함수 추가 (alt+shift+f 포맷팅)
+  delete = (item) => {
+    const thisItems = this.state.items;
+    const newItems = thisItems.filter(e => e.id !== item.id);
+    this.setState({ items: newItems }, () => {
+      //디버깅 콜백
+      console.log("Update Items : ", this.state.items)
+    });
+  }
+
   render() {
     // todoItems에 this.state.items.length 가 0보다 크다면 true 이므로 && 뒤에 값을 넘겨준다.
     // totoItem = this.state.items.length > 0 ? (<Paper></Paper>):"";  이렇게 해도 같은 결과이다. 조건선택문 ? ternary operator
@@ -39,7 +48,7 @@ class App extends React.Component {
 
     );
 
-    // (2) add 함수 연결 
+    // 생성된 컴포넌트 JSX를 리턴한다.
     return (
       <div className="App">
         <Container maxWidth="md">
--- src/Todo.js
+++ src/Todo.js
@@ -1,28 +1,67 @@
 import React from 'react';
-import {ListItem, ListItemText, InputBase, Checkbox} from "@material-ui/core";
+import {ListItem, ListItemText, InputBase, Checkbox, ListItemSecondaryAction, IconButton} from "@material-ui/core";
+import DeleteOutlined from "@material-ui/icons/DeleteOutlined";
 
 class Todo extends React.Component {
     constructor(props){
         super(props);
-        this.state ={ item: props.item };  // 매개변수 item 의 변수/값을 item에 대입
+        this.state ={ item: props.item, readOnly:true };  // 매개변수 item 의 변수/값을 item에 대입
+        this.delete = props.delete;
     }
 
+    deleteEventHandler =()=>{
+        this.delete(this.state.item);
+    }
+    offReadOnlyMode =()=>{
+        console.log("Event!",this.state.readOnly);
+        this.setState({readOnly:false},()=>{
+            console.log("ReadOnly?",this.state.readOnly)
+        });
+    }
+    enterKeyEventHandler =(e)=>{
+        if(e.key ==="Enter"){
+            this.setState({readOnly:true})
+        }
+    }
+    editEventHandler =(e)=>{
+        const thisItem =this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({item:thisItem});
+    }
+    checkboxEventHander =(e)=>{
+        console.log("chech box event call");
+        const thisItem = this.state.item;
+        thisItem.done = thisItem.done ?false :true;// thistItemdone = !thisitem.done
+        this.setState({item:thisItem});
+    }
     render(){
         const item =this.state.item;
         return (
             <ListItem>
-                <Checkbox checked={item.done}/>
+                <Checkbox checked={item.done}
+                onChange={this.checkboxEventHander}
+                />
                 <ListItemText>
                     <InputBase
-                    inputProps={{"aria-label":"naked"}}
+                    inputProps={{"aria-label":"naked", readOnly:this.state.readOnly}}
                     type="text"
                     id={item.id}
                     name={item.id}
                     value={item.title}
                     multiline={true}
                     fullWidth={true}
+                    onClick={this.offReadOnlyMode}
+                    onChange={this.editEventHandler}
+                    onkeyPress={this.enterKeyEventHandler}
                     />
                 </ListItemText>
+
+                <ListItemSecondaryAction>
+                    <IconButton aria-label="Delete"
+                    onClick={this.deleteEventHandler}>
+                     <DeleteOutlined />
+                    </IconButton>
+                </ListItemSecondaryAction>
             </ListItem>
         );
     }
